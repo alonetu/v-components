11c288c62db4aafa77d6b88c46e29a2b55e130f3
Alonetu <1217842903@qq.com>
2025-02-26 21:07:57 +0800

    feat: 组件测试

--- src/components/Button/Button.test.ts
+++ src/components/Button/Button.test.ts
@@ -1,6 +1,8 @@
-import { describe, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { mount } from '@vue/test-utils'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Button from './Button.vue'
+import Icon from '../Icon/Icon.vue'
 
 describe('Button.vue', () => {
   test('basic button', () => {
@@ -13,5 +15,63 @@ describe('Button.vue', () => {
       }
     })
     console.log(wrapper.html())
+    expect(wrapper.classes()).toContain('v-button--primary')
+    // slot
+    // 遍历 get, find
+    expect(wrapper.get('button').text()).toBe('button')
+    // events
+    wrapper.get('button').trigger('click')
+    console.log(wrapper.emitted())
+    expect(wrapper.emitted()).toHaveProperty('click')
+  })
+  test('disabled', () =>{
+    const wrapper = mount(Button, {
+      props: {
+        disabled: true
+      },
+      slots: {
+        default: 'disabled'
+      }
+    })
+    // attributes
+    expect(wrapper.attributes('disabled')).toBeDefined()
+    // attributes
+    expect(wrapper.find('button').element.disabled).toBeDefined()
+    wrapper.get('button').trigger('click')
+    expect(wrapper.emitted()).not.toHaveProperty('click')
+  })
+  test('icon', () => {
+    const wrapper = mount(Button, {
+      props: {
+        icon: 'arrow-up'
+      },
+      slots: {
+        default: 'icon'
+      },
+      global: {
+        stubs: ['FontAwesomeIcon']
+      }
+    })
+    const iconElement = wrapper.findComponent(FontAwesomeIcon)
+    expect(iconElement.exists()).toBeTruthy()
+    expect(iconElement.attributes('icon')).toBe('arrow-up')
+  })
+  test('loading', () => {
+    const wrapper = mount(Button, {
+      props: {
+        loading: true
+      },
+      slots: {
+        default: 'loading'
+      },
+      global: {
+        stubs: ['Icon']
+      }
+    })
+    console.log(wrapper.html())
+    const iconElement = wrapper.findComponent(Icon)
+    expect(iconElement.exists()).toBeTruthy()
+    expect(iconElement.attributes('icon')).toBe('spinner')
+    expect(wrapper.attributes('disabled')).toBeDefined()
   })
 })
